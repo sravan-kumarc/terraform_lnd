6014fc738d6b0f97bfc0ebf768baa7829579e4c4
SK <sravankumarchagalamarri@gmail.com>
2025-06-24 23:28:03 +0530

    variable-dev.tfvars

--- tfvars/main.tf
+++ tfvars/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "hydrogen" {
     ami = var.ec2_instance_ami
     instance_type = var.ec2_instance_type
-    subnet_id = "subnet-083b5bf951b0187cd"
+    subnet_id = "subnet-083b5bf951b0187cd" # 👈 Replace with your actual subnet ID
 
     tags = {
       Name = "HydrogenInstance"
